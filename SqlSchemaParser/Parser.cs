73bb6e3ed30747ba0edcaaed9e2ec05346e772a8
Russell Wallace <russell.wallace@gmail.com>
2023-11-27 12:16:16 +0000

    word

--- SqlSchemaParser/Parser.cs
+++ SqlSchemaParser/Parser.cs
@@ -33,50 +33,50 @@ public sealed class Parser {
 	void Lex() {
 		while (textIndex < text.Length) {
 			var end = textIndex + 1;
-			int t = text[textIndex];
-			switch (t) {
+			int k = text[textIndex];
+			switch (k) {
 			case '|':
 				switch (text[end]) {
 				case '|':
 					end = textIndex + 2;
-					t = kDoublePipe;
+					k = kDoublePipe;
 					break;
 				}
 				break;
 			case '!':
-				switch (t) {
+				switch (k) {
 				case '=':
 					end = textIndex + 2;
-					t = kNotEqual;
+					k = kNotEqual;
 					break;
 				case '<':
 					// https://stackoverflow.com/questions/77475517/what-are-the-t-sql-and-operators-for
 					end = textIndex + 2;
-					t = kGreaterEqual;
+					k = kGreaterEqual;
 					break;
 				case '>':
 					end = textIndex + 2;
-					t = kLessEqual;
+					k = kLessEqual;
 					break;
 				}
 				break;
 			case '>':
-				switch (t) {
+				switch (k) {
 				case '=':
 					end = textIndex + 2;
-					t = kGreaterEqual;
+					k = kGreaterEqual;
 					break;
 				}
 				break;
 			case '<':
-				switch (t) {
+				switch (k) {
 				case '=':
 					end = textIndex + 2;
-					t = kLessEqual;
+					k = kLessEqual;
 					break;
 				case '>':
 					end = textIndex + 2;
-					t = kNotEqual;
+					k = kNotEqual;
 					break;
 				}
 				break;
@@ -117,11 +117,105 @@ public sealed class Parser {
 			case ' ':
 				textIndex = end;
 				continue;
+			case 'A':
+			case 'B':
+			case 'C':
+			case 'D':
+			case 'E':
+			case 'F':
+			case 'G':
+			case 'H':
+			case 'I':
+			case 'J':
+			case 'K':
+			case 'L':
+			case 'M':
+			case 'O':
+			case 'P':
+			case 'Q':
+			case 'R':
+			case 'S':
+			case 'T':
+			case 'U':
+			case 'V':
+			case 'W':
+			case 'X':
+			case 'Y':
+			case 'Z':
+			case '_':
+			case 'a':
+			case 'b':
+			case 'c':
+			case 'd':
+			case 'e':
+			case 'f':
+			case 'g':
+			case 'h':
+			case 'i':
+			case 'j':
+			case 'k':
+			case 'l':
+			case 'm':
+			case 'n':
+			case 'o':
+			case 'p':
+			case 'q':
+			case 'r':
+			case 's':
+			case 't':
+			case 'u':
+			case 'v':
+			case 'w':
+			case 'x':
+			case 'y':
+			case 'z':
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				Word();
+				continue;
+			default:
+				// Common letters are handled in the switch for speed
+				// but there are other letters in Unicode
+				if (char.IsLetterOrDigit((char)k)) {
+					Word();
+					continue;
+				}
+
+				// Likewise whitespace
+				if (char.IsWhiteSpace((char)k)) {
+					textIndex = end;
+					continue;
+				}
+
+				throw Error("stray " + (char)k);
 			}
-			tokens.Add(new Token(textIndex, end, t));
+			tokens.Add(new Token(textIndex, end, k));
 			textIndex = end;
 		}
 		tokens.Add(new Token(textIndex, textIndex, -1));
+	}
+
+	void Word() {
+		var i = textIndex;
+		do
+			i++;
+		while (IsWordPart(text[i]));
+		tokens.Add(new Token(textIndex, i, kWord, text[textIndex..i]));
+		textIndex = i;
+	}
+
+	static bool IsWordPart(char c) {
+		if (char.IsLetterOrDigit(c))
+			return true;
+		return c == '_';
 	}
 
 	// Error functions return exception objects instead of throwing immediately
