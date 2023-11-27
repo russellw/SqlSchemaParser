6c2bc96f20cae1f13a1268624db6855ae90ba32e
Russell Wallace <russell.wallace@gmail.com>
2023-11-27 12:05:58 +0000

    format

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,38 +1,34 @@
 using SqlSchemaParser;
 
 namespace TestProject1;
-public class UnitTest1
-{
-    [Fact]
-    public void Blank()
-    {
-        Parser.Parse("");
-        Parser.Parse("\t");
-    }
+public class UnitTest1 {
+	[Fact]
+	public void Blank() {
+		Parser.Parse("");
+		Parser.Parse("\t");
+	}
 
-    [Fact]
-    public void LineComment()
-    {
-        Parser.Parse("--");
-        Parser.Parse("--\r\n--\r\n");
-    }
+	[Fact]
+	public void LineComment() {
+		Parser.Parse("--");
+		Parser.Parse("--\r\n--\r\n");
+	}
 
-    [Fact]
-    public void BlockComment()
-    {
-        Parser.Parse("/**/");
-        Parser.Parse("/***/");
-        Parser.Parse("/****/");
-        Parser.Parse("/*****/");
-        Parser.Parse("/* /*/");
-        Parser.Parse("/* /**/");
-        Parser.Parse("/* /***/");
-        Parser.Parse("/* /****/");
+	[Fact]
+	public void BlockComment() {
+		Parser.Parse("/**/");
+		Parser.Parse("/***/");
+		Parser.Parse("/****/");
+		Parser.Parse("/*****/");
+		Parser.Parse("/* /*/");
+		Parser.Parse("/* /**/");
+		Parser.Parse("/* /***/");
+		Parser.Parse("/* /****/");
 
-        var e = Assert.Throws<FormatException>(() => Parser.Parse("/*/"));
-        Assert.Matches(".*:1: ", e.Message);
+		var e = Assert.Throws<FormatException>(() => Parser.Parse("/*/"));
+		Assert.Matches(".*:1: ", e.Message);
 
-        e = Assert.Throws<FormatException>(() => Parser.Parse("\n/*"));
-        Assert.Matches(".*:2: ", e.Message);
-    }
+		e = Assert.Throws<FormatException>(() => Parser.Parse("\n/*"));
+		Assert.Matches(".*:2: ", e.Message);
+	}
 }
--- f.bat
+++ f.bat
@@ -1,4 +1,4 @@
 clang-format -i --style=file SqlSchemaParser\*.cs||exit /b
-rem clang-format -i --style=file TestProject1\*.cs||exit /b
+clang-format -i --style=file TestProject1\*.cs||exit /b
 call clean-cs -i -r .||exit /b
 git diff
