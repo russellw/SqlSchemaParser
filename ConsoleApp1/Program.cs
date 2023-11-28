ea8384fc17ed76a10777e83ae1cb90e453573ac6
Russell Wallace <russell.wallace@gmail.com>
2023-11-28 00:42:03 +0000

    clean

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,10 +2,5 @@ using SqlSchemaParser;
 
 class Program {
 	static void Main(string[] args) {
-		try {
-			Parser.Parse("/*");
-		} catch (Exception e) {
-			Console.WriteLine(e.Message);
-		}
 	}
 }
