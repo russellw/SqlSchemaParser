504169a43e1debab2df784625fd7c378e55839e1
Russell Wallace <russell.wallace@gmail.com>
2023-11-28 03:26:39 +0000

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,5 +2,9 @@ using SqlSchemaParser;
 
 class Program {
 	static void Main(string[] args) {
+		var schema = new Schema();
+		foreach (var file in args)
+			Parser.Parse(file, File.ReadAllText(file), schema);
+		Console.WriteLine(schema.IgnoredString());
 	}
 }
